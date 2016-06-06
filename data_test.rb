7780569953556841c723ceb2df22bcf6901b3d37
fushang318 <fushang318@gmail.com>
2016-06-06 12:36:01 +0800

    增加数据校验

---
+++ data_test.rb
@@ -0,0 +1,50 @@
+require "minitest/autorun"
+require 'yaml'
+
+describe "yaml" do
+  it "检验 yaml 数据格式" do
+    path = File.expand_path("../", __FILE__)
+    Dir[File.join(path, "*.yml")].each do |yaml_file|
+      p yaml_file
+      yaml = YAML.load_file yaml_file
+      yaml.is_a?(Array).must_equal true
+      yaml.each do |item|
+        p item
+        item.is_a?(Hash).must_equal true
+
+        ["essay", "multi_choice", "single_choice", "bool","file_upload"].include?(item["kind"]).must_equal true
+
+        ["text", "md"].include?(item["content_format"]).must_equal true
+        if item["content"].include?("`")
+          (item["content_format"] == "md").must_equal true
+        end
+
+        item["content"].is_a?(String).must_equal true
+
+        if item["kind"] == "essay"
+          item.keys.count.must_equal 3
+        end
+
+        if item["kind"] == "multi_choice"
+          item.keys.count.must_equal 4
+          item["answer"].is_a?(Hash).must_equal true
+          item["answer"]["choices"].is_a?(Array).must_equal true
+          item["answer"]["corrects"].is_a?(Array).must_equal true
+        end
+
+        if item["kind"] == "single_choice"
+          item.keys.count.must_equal 4
+          item["answer"].is_a?(Hash).must_equal true
+          item["answer"]["choices"].is_a?(Array).must_equal true
+          item["answer"]["correct"].is_a?(String).must_equal true
+        end
+
+        if item["kind"] == "bool"
+          item.keys.count.must_equal 4
+          [true, false].include?(item["answer"]).must_equal true
+        end
+
+      end
+    end
+  end
+end
